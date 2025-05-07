c987ad21b70610e3c17203c076098c4dda067490
ELEANA MAE B. ESPARAGOZA <embe2023-2586-11581@bicol-u.edu.ph>
2025-05-07 17:00:23 +0800

    Update and rename admin-view-report.php to adminviewreports.php

---
+++ tolosabh/adminviewreports.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Account Information Dashboard</title>
+  <link rel="stylesheet" href="adminviewreports.css">
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <img src="IMG/TOLOSA.png" alt="Logo">
+    </div>
+
+    <nav class="nav-bar">
+      <button class="nav-btn">Resident Services</button>
+      <button class="nav-btn">Community Building</button>
+      <button class="nav-btn">View Invoices</button>
+      <button class="nav-btn">About Us</button>
+    </nav>
+
+    <button class="profile-button" onclick="alert('Profile clicked!')">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-person-circle" viewBox="0 0 16 16">
+      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+      <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+    </svg>
+  </button>
+  </header>
+
+  <main>
+    <h1>USER REPORTS</h1>
+    <div class="report-table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Tenant Name</th>
+            <th>Subject</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </main>
+</body>
+</html>
+
+    </div>
+  </header>
+  
+          
