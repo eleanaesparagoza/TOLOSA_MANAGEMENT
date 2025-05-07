3bb7f3bc74282ba6be9756c0eca4193fc6a91ba8
allanaahhh <ajcb2023-7485-41600@bicol-u.edu.ph>
2025-05-07 14:50:32 +0800

    Update and rename noticeboard.php to noticeboard1.php
    (ok na toh)

---
+++ tolosabh/noticeboard1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Notice Board</title>
+  <link rel="stylesheet" href="notice1.css">
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div class="logo">
+      <img src="IMG/TOLOSA.png" alt="Logo">
+    </div>
+
+    <nav class="nav-bar">
+      <button class="nav-btn">Resident Services</button>
+      <button class="nav-btn">Community Building</button>
+      <button class="nav-btn">View Invoices</button>
+      <button class="nav-btn">About Us</button>
+    </nav>
+
+    <button class="profile-button" onclick="alert('Profile clicked!')">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-person-circle" viewBox="0 0 16 16">
+      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+      <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+    </svg>
+  </button>
+  </header>
+
+    </div>
+  </header>
+  <main>
+    <section class="notice-board">
+      <h2>DIGITAL NOTICE BOARD</h2>
+      <h3>NOTICE OF PAYMENT</h3>
+      <div class="notice">
+        <p>DUE DATE: February 12, 2025</p>
+        <p>AMOUNT: $100</p>
+        <p>NOTE: If rent is overdue, there is a penalty charge of $20</p>
+      </div>
+    </section>
+
+    <section class="community-forum">
+  <h2>APARTMENT COMMUNITY FORUM</h2>
+  <p>Please state your feedback or issues here. Thank you!</p>
+  <textarea placeholder="Enter Text"></textarea>
+  <button>Submit</button>
+</section>
+
+  </main>
+</body>
+</html>
