5e3085f27c344ceb2e41f355385026e46d2d1ba6
ELEANA MAE B. ESPARAGOZA <embe2023-2586-11581@bicol-u.edu.ph>
2025-05-14 11:10:54 +0800

    Create regacc.php

---
+++ tolosabh/regacc.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="reg.css">
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,100..700&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <img src="IMG/TOLOSA.png" alt="Logo">
+    </div>
+
+    <nav class="nav-bar">
+      <a href="#" class="nav-btn">Resident Services</a>
+      <a href="#" class="nav-btn">Community Building</a>
+      <a href="#" class="nav-btn">View Invoices</a>
+      <a href="#" class="nav-btn">About Us</a>
+    </nav>
+
+    <div class="header-right">
+      <button class="auth-btn">Login</button>
+    </div>
+  </header>
+
+  <main>
+    <h1>REGISTER NOW</h1>
+    <div class="register-box">
+      <form>
+        <label>First Name</label>
+        <input type="text" placeholder="Value">
+
+        <label>Last Name</label>
+        <input type="text" placeholder="Value">
+
+        <label>Contact No.</label>
+        <input type="text" placeholder="Value">
+
+        <label>Address</label>
+        <input type="text" placeholder="Value">
+
+        <label>Email</label>
+        <input type="email" placeholder="Value">
+
+        <label>Create Password</label>
+        <input type="password" placeholder="Value">
+
+        <button type="submit">Register</button>
+      </form>
+      <p>If you have created an account, please log in here:</p>
+      <a href="#" class="login-link">Log In</a>
+    </div>
+  </main>
+</body>
+</html>
